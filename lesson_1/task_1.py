3db4c88e3a5f7a3b4f174e2e2f6a8b52dabecee6
red_r2005 <red_r2005@mail.ru>
2023-05-21 14:19:03 +0300

    First commit

---
+++ lesson_1/task_1.py
@@ -0,0 +1,10 @@
+a = input('Input side "a" size:' )
+b = input('Input side "b" size:' )
+c = input('Input side "c" size:' )
+
+if a == b and a == c and b == c:
+    print(True)
+else:
+    print(False)
+
+
---
+++ lesson_1/task_2.py
@@ -0,0 +1,6 @@
+N = int(input('Input digit: '))
+
+if N >= 10 and N <= 99:
+    print(True)
+else:
+    print(False)
