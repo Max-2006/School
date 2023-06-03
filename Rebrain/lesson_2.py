1cc5a6c5979720bd067e8c8d6ec4ac7d6136f690
red_r2005 <red_r2005@mail.ru>
2023-06-03 21:47:12 +0300

    самостоятельно выполнил задание 1 страница 235

--- Rebrain/lesson_2.py
+++ Rebrain/lesson_2.py
@@ -1,5 +1,9 @@
 # 'May 24 12:48:31 ideapad systemd[1]: logrotate.service: Succeeded.'
 
-user_log = 'May 24 12:48:31 ideapad systemd[1]: logrotate.service: Succeeded.'
+User_log = 'May 24 12:48:31 ideapad systemd[1]: logrotate.service: Succeeded.'
 
-print(len('failed'))
+time1 = User_log[7:9]
+time2 = User_log[10:12]
+time3 = User_log[13:15]
+summa = int(time1) + int(time2) + int(time3)
+print(summa)
